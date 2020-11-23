b1fa680eb7c7cee582f1c979c69b16036f1d3d05
riabow <63100171+riabov-vladimir@users.noreply.github.com>
2020-11-23 18:45:58 +0300

    Initial commit

---
+++ myshop/urls.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from shop import views
+from users.views import register
+import django.contrib.auth.urls as accounts
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('cart/', include('cart.urls', namespace='cart')),
+    path('orders/', include('orders.urls', namespace='orders')),
+    path('shop/', include('shop.urls', namespace='shop')),
+    path('', views.IndexArticle.as_view(), name='index'),
+    path('register/', register, name='register'),
+    path('', include(accounts)),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
---
+++ users/views.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render
+
+from django.shortcuts import render, redirect
+
+from users.forms import UserAdminCreationForm
+
+
+def register(req):
+    form = UserAdminCreationForm()
+    if req.method == 'POST':
+        form = UserAdminCreationForm(req.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('register')
+    return render(req, 'register.html', {'form': form})
