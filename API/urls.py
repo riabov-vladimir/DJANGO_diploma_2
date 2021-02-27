ff4896d823a63afe3e9e25f445fd7d41b14fa42d
Wolodia <Riabow>
2021-02-27 12:40:49 +0300

    ['GET', 'PUT', 'DELETE'] combined

--- API/urls.py
+++ API/urls.py
@@ -5,6 +5,6 @@ app_name = 'API'
 
 urlpatterns = [
     path('<id>/', api_detail_category_view, name='category_detail'),
-    path('', api_all_category_view, name='category_detail'),
-    path('delete/<id>/', api_delete_category_view, name='category_delete'),
+    path('', api_all_category_view, name='category'),
+    # path('delete/<id>/', api_delete_category_view, name='category_delete'),
 ]
--- API/views.py
+++ API/views.py
@@ -5,7 +5,7 @@ from shop.models import *
 from API.serializers import CategorySerializer
 
 
-@api_view(['GET'])
+@api_view(['GET', 'PUT', 'DELETE'])
 def api_detail_category_view(request, id):
     print('api_detail_category_view')
     try:
@@ -13,8 +13,23 @@ def api_detail_category_view(request, id):
     except Category.DoesNotExists:
         return Response(status=status.HTTP_404_NOT_FOUND)
 
-    serializer = CategorySerializer(category)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        serializer = CategorySerializer(category)
+        return Response(serializer.data)
+
+    if request.method == 'PUT':
+        serializer = CategorySerializer(category, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(status=status.HTTP_200_OK, data=f'Запись номер {id} успешно обновлена')
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    if request.method == 'DELETE':
+        object_deletion = category.delete()
+        if object_deletion:
+            return Response(status=status.HTTP_200_OK, data=f'Запись номер {id} успешно удалена')
+        return Response(status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(['GET'])
@@ -29,32 +44,27 @@ def api_all_category_view(request):
     return Response(serializer.data)
 
 
-@api_view(['PUT'])
-def api_detail_category_view(request, id):
-    print('api_detail_category_view')
-    try:
-        category = Category.objects.get(id=id)
-    except Category.DoesNotExists:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-
-    serializer = CategorySerializer(category, data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(status=status.HTTP_200_OK, data=f'Запись номер {id} успешно обновлена')
-
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+# @api_view(['PUT'])
+# def api_detail_category_view(request, id):
+#     print('api_detail_category_view')
+#     try:
+#         category = Category.objects.get(id=id)
+#     except Category.DoesNotExists:
+#         return Response(status=status.HTTP_404_NOT_FOUND)
 
 
-@api_view(['DELETE'])
-def api_delete_category_view(request, id):
-    print('api_delete_category_view')
-    try:
-        category = Category.objects.de(id=id)
-    except Category.DoesNotExists:
-        return Response(status=status.HTTP_404_NOT_FOUND)
 
-    object_deletion = category.delete()
-    if object_deletion:
-        return Response(status=status.HTTP_200_OK, data=f'Запись номер {id} успешно удалена')
-
-    return Response(status=status.HTTP_400_BAD_REQUEST)
+#
+# @api_view(['DELETE'])
+# def api_delete_category_view(request, id):
+#     print('api_delete_category_view')
+#     try:
+#         category = Category.objects.de(id=id)
+#     except Category.DoesNotExists:
+#         return Response(status=status.HTTP_404_NOT_FOUND)
+#
+#     object_deletion = category.delete()
+#     if object_deletion:
+#         return Response(status=status.HTTP_200_OK, data=f'Запись номер {id} успешно удалена')
+#
+#     return Response(status=status.HTTP_400_BAD_REQUEST)
